9140a9f85bbfd79f49594f37ee05a1fa9a07e527
Kusum <Kusum-Thapa@users.noreply.github.com>
2020-12-16 11:40:03 +0000

    Added Landing Page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="mui-appbar mui--z1 purple-col">
+
+        <div className="mui-container">
+          <table>
+            <tr className="mui--appbar-height">
+              <td className="mui--text-title">Help Who Needs</td>
+              <td className="mui--text-right">
+                <ul className="mui-list--inline mui--text-body2">
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </td>
+            </tr>
+          </table>
+        </div>
       </header>
+      <div id="content-wrapper" className="mui--text-center">
+
+        <div className="mui--appbar-height"></div>
+        <h1>Help Who Needs</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere blanditiis aspernatur animi, voluptatem
+        labore nostrum nihil modi temporibus error eum!
+        Molestiae voluptas voluptatem possimus libero cupiditate neque facilis architecto obcaecati.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempore dolore deleniti quaerat,
+        doloribus consequuntur.
+        Facere dicta, minus temporibus in laborum, repellat praesentium maxime eum excepturi autem non!
+        Obcaecati, veritatis!
+    </p>
+        <button className="mui-btn mui-btn--raised" onclick="location.href='Login.html'">I need Help</button>
+        <button className="mui-btn mui-btn--raised" onclick="location.href='Registration.html'">I can Help</button>
+        <div className="push"></div>
+      </div>
+      <footer>
+        <div className="mui-container mui--text-center">
+          Made by <a href="https://github.com/YourTechAngels">YourTechAngels</a>
+        </div>
+      </footer>
     </div>
   );
 }
